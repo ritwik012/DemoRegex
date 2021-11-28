d0f6f360ccc4def556d1f8588eb0f9e02342d894
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-28 23:35:42 +0530

    [Rutwik]Added:PinCodeValidation File

---
+++ Regex/Patterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DemoRegex
+{
+    class Patterns
+    {
+        public static string REGEX_PINCODE1 = "^[1-9][0-9]{5}$";
+        public static string REGEX_PINCODE2 = "^[1-9][0-9]{2} [0-9]{3}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE1);
+        }
+        public bool validatePin(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE2);
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DemoRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose the program to be executed : \n 1. PinCode Validation \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(patterns.validatePinCode("400008"));
+                        Console.WriteLine(patterns.validatePinCode("A400008"));
+                        Console.WriteLine(patterns.validatePinCode("400008B"));
+                        Console.WriteLine(patterns.validatePin("400 008"));
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
